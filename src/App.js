7ea1c828f365806c64f060655a6f08b998e8e880
Mark Reed <markdreed512@gmail.com>
2020-03-01 17:56:55 -0600

    styled comp config

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Navbar from './components/navbar/Navbar'
-function App() {
-  return (
-    <div className="App">
-      <Navbar></Navbar>
-    </div>
-  );
+import Globalstyles from './styles/Global'
+
+class App extends Component() {
+  state = {
+    navbarOpen: false
+  }
+  handleNavBar = () => {
+    this.setState({navbarOpen: !this.state.navbarOpen})
+  }
+  render() {
+    return (
+      <>
+        <Navbar
+          navbarState={this.state.navbarOpen} 
+          handleNavbar={this.handleNavbar}
+        />
+        <Globalstyles/>
+      </>
+    )
+  }
+
 }
 
 export default App;
