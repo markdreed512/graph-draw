9757e45fd9e977b2f583eef8bb35ef5e4f9b1557
Mark Reed <markdreed512@gmail.com>
2020-03-01 17:10:13 -0600

    navbar initial commit

---
+++ src/components/Nav.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Nav(){
+    return (
+        <div>
+            <ul>
+                <li>Home</li>
+                <li>My Images</li>
+                <li>Help</li>
+            </ul>
+        </div>
+
+    )
+}
+export default Nav;
