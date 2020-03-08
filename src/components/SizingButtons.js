2fd07022feecc305d0254bb05400391f9ce3b24d
Mark Reed <markdreed512@gmail.com>
2020-03-08 16:25:26 -0500

    sizing button labels

--- src/components/SizingButtons.js
+++ src/components/SizingButtons.js
@@ -1,28 +1,36 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styled from 'styled-components'
 
 function SizingButtons(){
+    const [cellSize, setCellSize] = useState(100)
+    const increaseSize = () => {
+        setCellSize(cellSize + 2)
+    }
+    const decreaseSize = () => {
+        setCellSize(cellSize - 2)
+    }
     const Div = styled.div`
         margin: 100px 0 0 300px;
         float: left;
         font-family: arial;
+        color: white;
     `
     const Button = styled.button`
         font-size: 50px;
         background-color: green;
         display: block;
         width: 100px;
-
     `
     const P = styled.p`
         color: white;
     `
     return (
-        <Div>
-            <Button>+</Button>
-            <P>Increase # of Cells</P>
-            <Button>-</Button>
-            <P>Decrease # of Cells</P>
+        <Div >
+            <Button onClick = { increaseSize }>+</Button>
+            <P>Increase Cell Size</P>
+            <Button onClick = { decreaseSize }>-</Button>
+            <P>Decrease Cell Size</P>
+    <h1>Current Cell Size: {cellSize}</h1>
         </Div>
     )
 }
